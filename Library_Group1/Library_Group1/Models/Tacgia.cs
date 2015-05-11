8f29a53147d0714ebe938ca995ff8511e8000990
hoanganhquan <quanhv_b01023@fpt.aptech.ac.vn>
2015-05-11 10:31:40 +0700

    Quan tao class Tacgia

---
+++ Library_Group1/Library_Group1/Models/Tacgia.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Library_Group1.Models
+{
+    public class Tacgia
+    {
+        public string MaTG { get; set; }
+        public string TenTG { get; set; }
+        public string Diachi { get; set; }
+        public string SoDT { get; set; }
+        public string Email { get; set; }
+    }
+}
