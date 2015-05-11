279f2e61e7c9e28542b193865adb0f3d7733122d
Thao <thaohtb01421@fpt.edu.vn>
2015-05-11 11:06:04 +0700

    Thao tao BangSach

---
+++ Library_Group1/Library_Group1/Models/BangSach.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Library_Group1.Models
+{
+    public class BangSach
+    {
+        public string MaSach { set; get; }
+        public string TenSach { set; get; }
+        public string TomTat { get; set; }
+        public string MaLoai { get; set; }
+        public string MaTG {get; set;}
+        public string MaNXB { get; set; }
+    }
+}
